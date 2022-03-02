d4d99496179d8d664b6330c9e78e8322d6979822
marc <marc@DEV-MPI>
2022-03-02 16:02:20 +0100

    fix on_files arguments

--- mkdocs_awesome_pages_plugin/plugin.py
+++ mkdocs_awesome_pages_plugin/plugin.py
@@ -49,7 +49,7 @@ class AwesomePagesPlugin(BasePlugin):
             print("Awesome_page: env var set " + variable_name)
             pc.State[variable_name] = " "
 
-    def on_files(files, config):
+    def on_files(files: Files, config: Config):
         to_removes = []
         for file in files:            
             if file.is_documentation_page():
