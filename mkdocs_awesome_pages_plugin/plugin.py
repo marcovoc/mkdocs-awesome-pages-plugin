ea0c55ada16fe87850338bf2d1da8068f0e52fab
marc <marc@DEV-MPI>
2022-03-14 16:28:10 +0100

    fix

--- mkdocs_awesome_pages_plugin/plugin.py
+++ mkdocs_awesome_pages_plugin/plugin.py
@@ -91,7 +91,7 @@ class AwesomePagesPlugin(BasePlugin):
             for match in re.finditer(regex_link, html):
                 if not match.group()[0].lower().endswith(".html"):
                     print("Awesome_page: on_page_content catch " + match.groups()[0])
-                    self.REFERENCED_FILES_EXCEPT_HTML.append(os.path.normpath(os.path.join(file_dirname, match.groups()[1])))
+                    self.REFERENCED_FILES_EXCEPT_HTML.append(os.path.normpath(os.path.join(file_dirname, match.groups()[0])))
 
     def on_post_build(self, config: Config):
         to_removes = []
