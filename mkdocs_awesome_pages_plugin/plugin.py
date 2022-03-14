e4b9a2ad805ff30a3dae635329c4fa8071bbcae2
marc <marc@DEV-MPI>
2022-03-14 16:18:55 +0100

    debug

--- mkdocs_awesome_pages_plugin/plugin.py
+++ mkdocs_awesome_pages_plugin/plugin.py
@@ -89,8 +89,7 @@ class AwesomePagesPlugin(BasePlugin):
         if found:
             file_dirname = os.path.dirname(page.file.abs_dest_path)
             for match in re.finditer(regex_link, html):
-                if not match.group()[0].lower().endswith(".html"):
-                    print(html)
+                if len(match.groups()) > 1 and not match.group()[0].lower().endswith(".html"):
                     print("Awesome_page: on_page_content catch " + match.group()[0])
                     self.REFERENCED_FILES_EXCEPT_HTML.append(os.path.normpath(os.path.join(file_dirname, match.groups()[1])))
 
